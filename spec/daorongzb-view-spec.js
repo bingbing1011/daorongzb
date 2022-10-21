7f10637917190b07a22156da01ee50a07d8a6a6b
337651121@qq.com <337651121@qq.com>
2022-10-21 17:11:07 +0800

    haha

--- spec/daorongzb-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import DaorongzbView from '../lib/daorongzb-view';
-
-describe('DaorongzbView', () => {
-  it('has one valid test', () => {
-    // expect('life').toBe('easy');
-  });
-});
