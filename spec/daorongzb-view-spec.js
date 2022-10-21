8ea786ce90301e9cb53b7123b47a3d87fdb4e855
337651121@qq.com <337651121@qq.com>
2022-10-21 16:44:07 +0800

    first commit

---
+++ spec/daorongzb-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DaorongzbView from '../lib/daorongzb-view';
+
+describe('DaorongzbView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
